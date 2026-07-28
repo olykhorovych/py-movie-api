9f77f1020aa74a3d5526a9bd7ac395b63d73096f
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-07-28 23:06:53 +0300

    fix: solve required changes

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/cinema/", include("cinema.urls"), name="cinema"),
+    path("api/cinema/", include("cinema.urls"), name="cinema"),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -18,7 +18,7 @@ def movie_list(request: Request) -> Response:
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -31,6 +31,7 @@ def movie_detail(request: Request, pk: int) -> Response:
         serializer = MovieSerializer(movie, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
     else:
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
